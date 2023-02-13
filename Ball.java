59bf5a5a030e18d5a12eb1e8849f29fa955962f5
vie <paul.vie@etu.u-paris.fr>
2023-02-13 13:31:25 +0100

    Correction angle rebond
    decalement du plan euclidien

--- Ball.java
+++ Ball.java
@@ -20,7 +20,7 @@ public class Ball{
     private boolean ispresent = true;
     private boolean atoucher = false;
 
-    private double g=100; // m/s
+    private double g=300; // m/s
     private double coeffRebond = 0.8;
 
     private GameView court;
@@ -95,8 +95,8 @@ public class Ball{
 
             double ux = r.x0 - r.x1;
             double uy = r.y0 - r.y1;
-            double vx = ballSpeedX+p1;
-            double vy = ballSpeedY+p2;
+            double vx = ballSpeedX;
+            double vy = ballSpeedY;
 
             double s = ux*vx+ uy*vy;
             double d1 = Math.sqrt(ux* ux + uy*uy);
@@ -104,11 +104,21 @@ public class Ball{
             double alpha =Math.acos(s/(d1*d2));
             System.out.println("angle d'incidence : " + alpha*180/Math.PI);
             System.out.println("touche");
+            System.out.println("vitesse en x : " + ballSpeedX);
+            System.out.println("vitesse en y : " + ballSpeedY);
             atoucher = true;
             double beta = Math.PI - alpha;
-            double angle = -beta;
+            double angle = beta - r.angle;
+            if (beta < 0){
+                beta = -beta;
+            }
+            System.out.println("angle de rebond : " + angle*180/Math.PI);
+            // calcul de la nouvelle vitesse
             ballSpeedX=d2*Math.cos(angle);
             ballSpeedY=d2*Math.sin(angle);
+            System.out.println("vitesse en x : " + ballSpeedX);
+            System.out.println("vitesse en y : " + ballSpeedY);
+            System.out.println("vitesse totale : " + Math.sqrt(ballSpeedX*ballSpeedX + ballSpeedY*ballSpeedY));
 
         } else {}
         ballX = nextBallX;
@@ -126,204 +136,9 @@ public class Ball{
     
 
 
-    // // public Rectangle touchedWall(double nextBallX, double nextBallY){
-    // //     Rectangle r=null;
-    // //     System.out.println("no");
-    // //     for (Rectangle rect:court.getRectangle()){
-    // //         if ((int) rect.x0 <= (int)  ballX &&  (int) ballX <= (int)  rect.x1
-    // //         &&  (int)  rect.y0 <= (int)  ballY && (int)  ballY <= (int)  rect.y1) {
-
-
-               
-    // //         double x = rect.x0 + nextBallX-rect.x0;
-    // //         double adjacent = rect.x1 - x;
-    // //         double tan = Math.tan(rect.angle);
-    // //         double y = rect.y1 - tan*adjacent;
-    // //         // System.out.println((nextBallX+ballSpeedX) + " "+ (nextBallY+ballSpeedY));
-            
-    // //         if ((ballY+ballSpeedY) >= y && (ballX+ballSpeedX) <= x) return rect;
-    // //             // System.out.println(x + " " + y);
-    // //             // System.out.println(ballX + " " + ballY);
-    // //         // if (x<= 2*nextBallX-ballSpeedX && y <= 2*nextBallY+ballSpeedY) {
-    // //         //         System.out.println("touch");
-    // //         //         //return rect;
-    // //         //     }
-    // //         }
-
-    // //         // double vRectX = r.x0 - r.x1;
-    // //         // double vRectY = r.y0 - r.y1;
-    // //         // double vVitX = nextBallX-ballSpeedX;
-    // //         // double vVitY = nextBallY-ballSpeedY;
-    // //         // double e1 = vVitX - r.x0 / vRectX;
-    // //         // double e2 = vVitY - r.y0 / vRectY;
-    // //         // if (e1==e2) 
-
-            
-    // //         }
-    // //     return r;
-
-    // // }
-
-    // public Rectangle touchedWall(double nextBallX, double nextBallY,double deltaT){
-    //     Rectangle r=null;
-    //     //System.out.println("-------------------------------------------");
-        
-    //     for (Rectangle rect:court.getRectangle()){   
-    //         System.out.println("'-------------------------'");
-    //         if (Math.min(rect.x0,rect.x1) <= nextBallX && nextBallX <= Math.max(rect.x0,rect.x1) &&
-    //         Math.min(rect.y0,rect.y1)<= nextBallY && nextBallY <= Math.max(rect.y0,rect.y1)) {
-    //                 double nextBallX2=nextBallX;
-    //                 double nextBallY2=nextBallY;
-    //                 double x = Math.min(rect.x0,rect.x1) + nextBallX2-Math.min(rect.x0,rect.x1);
-    //                 double adjacent = x- Math.min(rect.x0,rect.x1);
-    //                 System.out.println(adjacent);
-    //                 double tan = Math.tan(-rect.angle);
-    //                 System.out.println(tan*adjacent);
-    //                 double y = Math.min(rect.y1,rect.y0) + tan*adjacent;
-    //                 System.out.println(x + " " + y);
-                    
-    //                 for (int i=0;i<1;i++){
-    //                         System.out.println(i + " " +  nextBallX2 + " " + nextBallY2);
-    //                     if (ballSpeedY>=0){
-                            
-    //                         if (ballSpeedX>=0){
-    //                             if (nextBallX2+ballRadius>=x && nextBallY2+ballRadius>=y) {
-                                    
-    //                                 return rect;}
-    //                         }
-    //                         else {
-    //                             if (nextBallX2<=x && nextBallY2+ballRadius>=y) {
-                                    
-    //                                 return rect;}
-    //                         }
-    //                     }
-    //                     // else {
-                        
-    //                     //     if (ballSpeedX>=0){
-    //                     //         if (nextBallX2>=x && nextBallY2<=y) {
-                                    
-    //                     //             return rect;}
-    //                     //     }
-    //                     //     else {
-    //                     //         if (nextBallX2<=x && nextBallY2>=y) {
-    //                     //             return rect;}
-    //                     //     }
-    //                     // }
-                        
-    //                     nextBallX2 = nextBallX2 + deltaT * ballSpeedX;
-    //                     nextBallY2 = nextBallY2 + deltaT * ballSpeedY + 1/2*g*(deltaT*deltaT);
-    //             }
-
-    //         }              
-    //     } 
-        
-    //     return r;
-    // }
-
-    // public Rectangle touchedWall(double nextBallX, double nextBallY,double nextBallSpeedX, double nextBallSpeedY,double deltaT){
-    //     Rectangle r=null;
-    //     int j=0;
-    //     System.out.println("-------------------------------------------");
-    //     double nextBallX2=nextBallX;
-    //     double nextBallY2=nextBallY;
-    //     for (Rectangle rect:court.getRectangle()){     
-    //         if (Math.min(rect.x0,rect.x1) <= nextBallX && nextBallX <= Math.max(rect.x0,rect.x1) &&
-    //         Math.min(rect.y0,rect.y1) <= nextBallY && nextBallY <= Math.max(rect.y0,rect.y1)) {
-    //             System.out.println(rect.toString());
-    //             for (int i=0;i<1;i++){
-    //                 double ux = rect.x0 - rect.x1;
-    //                 double uy = rect.y0 - rect.y1;
-    //                 double vx = nextBallSpeedX;
-    //                 double vy = nextBallSpeedY;
-    
-
-    
-    
-    //                 double coeffDirecteurDroite = uy/ux;
-    //                 double b1 = rect.y0 - coeffDirecteurDroite*rect.x0;
-                    
-    
-    //                 double coeffDirecteurVecteur = vy/vx;
-    //                 double b2 = (nextBallY2 + ballRadius)- coeffDirecteurVecteur*(nextBallX2 + ballRadius) ;
-    
-    //                 if (coeffDirecteurDroite!=coeffDirecteurVecteur){
-    //                     double x,y;
-    //                     x = (b2-b1)/(coeffDirecteurDroite-coeffDirecteurVecteur);
-    //                     y = (-b2*coeffDirecteurDroite+b1*coeffDirecteurVecteur)/(coeffDirecteurVecteur-coeffDirecteurDroite);
-    //                     this.x = (b2-b1)/(coeffDirecteurDroite-coeffDirecteurVecteur);
-    //                     this.y = (-b2*coeffDirecteurDroite+b1*coeffDirecteurVecteur)/(coeffDirecteurVecteur-coeffDirecteurDroite);
-    //                     //System.out.println(j);
-    //                     if (nextBallSpeedX>=0 && x<=nextBallX2) return null;
-    //                     if (nextBallSpeedX<=0 && x>=nextBallX2) return null;
-    //                     if (nextBallSpeedY>=0 && y<=nextBallY2) return null;
-    //                     if (nextBallSpeedY<=0 && y>=nextBallY2) return null;
-    //                     if (Math.min(rect.x0,rect.x1) <= x && x <= Math.max(rect.x0,rect.x1)
-    //                     && Math.min(rect.y0,rect.y1)<= y && y <= Math.max(rect.y0,rect.y1)){
-    //                         System.out.println(x+" "+y);
-    //                         System.out.println(nextBallX2+ballRadius+" "+nextBallY2+ballRadius);
-    //                         if (((int) nextBallX2+ballRadius)==((int)x) && ((int)nextBallY2+ballRadius)==((int)y)) return rect;
-                            
-    //                             //System.out.println(nextBallX2 + " "+ nextBallY);
-    //                             if (nextBallSpeedY>=0){
-    //                                 System.out.println(1);             
-    //                                 if (nextBallSpeedX>=0){
-    //                                     System.out.println(1);
-    //                                     System.out.println(((int) nextBallX2+ballRadius*2) + ">=" +  ((int) x) + " " + ((int) nextBallY2+ballRadius*2) + ">=" + ((int) y));
-    //                                     if (((int) nextBallX2+ballRadius*2) >= ((int) x) && ((int) nextBallY2+ballRadius*2)>=((int) y)) {
-    //                                         System.out.println(1);
-    //                                         return rect;
-    //                                     }
-    //                                 }
-    //                                 else {
-    //                                     System.out.println(((int) nextBallX2+ballRadius) + "<=" +  ((int) x) + " " + ((int) nextBallY2+ballRadius*2) + ">=" + ((int) y));
-    //                                     if (((int) nextBallX2) <= ((int) x) && ((int) nextBallY2+ballRadius*2)>=((int) y)) {
-    //                                         return rect;
-    //                                     }
-    //                                     }
-    //                             }
-    //                             else {
-    //                                 System.out.println(2);
-    //                                 if (nextBallSpeedX>=0){
-    //                                     System.out.println(nextBallX2+ballRadius*2 +  ">=" + x + " " + nextBallY2 + "<=" + y );
-    //                                     if (((int) nextBallX2+ballRadius*2) >= ((int) x) && ((int) nextBallY2)<=((int) y)) {
-    //                                         return rect;
-    //                                     }
-    //                                     }
-    //                                     else {
-    //                                         System.out.println(2);
-    //                                         System.out.println(((int) nextBallX2) + "<=" +  ((int) x) + " " + ((int) nextBallY2) + ">=" + ((int) y));
-    //                                         if (((int) nextBallX2) <= ((int) x) && ((int) nextBallY2)>=((int) y)) {
-    //                                             return rect;
-    //                                         }
-    //                                         }
-    //                                     }
-                                                    
-
-    //                         }
-    //                     }
-
-    //             }
-    //         }
-                    
-    //             }
-    //         j++;  
-
-    //     return r;
-    // }
 
     public Rectangle touchedWall(double deltaT){
         Rectangle r=null;
-        // double nextBallX2= nextBallX + deltaT * nextBallSpeedX;
-        // double nextBallY2 = nextBallY + deltaT * nextBallSpeedY + 1/2*g*(deltaT*deltaT);
-        // double nextBallSpeedY2= nextBallSpeedY + g*deltaT;
-        // double nextBallSpeedX2=  nextBallSpeedX;
-
-
-        // double nextBallX3 = nextBallX2 + deltaT * nextBallSpeedX2;
-        // double nextBallY3 = nextBallY2 + deltaT * nextBallSpeedY2 + 1/2*g*(deltaT*deltaT);
-        // double nextBallSpeedY3= nextBallSpeedY2 + g*deltaT;
-        // double nextBallSpeedX3=  nextBallSpeedX2;
-
         for (Rectangle rect:court.getRectangle()){     
             if (Math.min(rect.x0,rect.x1) <= nextBallX && nextBallX <= Math.max(rect.x0,rect.x1) &&
             Math.min(rect.y0,rect.y1) <= nextBallY && nextBallY <= Math.max(rect.y0,rect.y1)) { // Je regarde si ma balle est dans la surface de mon trait/rectangle 
