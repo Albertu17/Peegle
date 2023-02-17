adaea6127f839ce8b254dbed67201d57516b16ed
Thibault Poux <thibault.poux@etu.u-paris.fr>
2023-02-17 22:12:04 +0100

    Affichage du jeux en grand ecran

--- Vue/GameView.java
+++ Vue/GameView.java
@@ -62,33 +62,22 @@ public class GameView extends JPanel implements MouseInputListener{
     setLayout(null);
     setVisible(true);  
 
+    
     // creation canon :
-    canon = new Canon(this) ;
-    this.add(canon) ;
-    canon.setVisible(true);
-    canon.setBalleATirer(new Ball(0, 0, 0, 0, this));
-
-
-    // balls.add(new Ball(225,300,0,0,this));
-    // rectanlgle.add(new Rectangle(200, 400, 100, -45));
-    // balls.add(new Ball(20,30,10,-10,this));
-
-    balls.add(new Ball(60,10,75,10,this));
-    balls.add(new Ball(100,30,-65,10,this));
-
-    // balls.add(new Ball(200,10,-100,10,this));
-    // balls.add(new Ball(250,30,10,100,this));
-
-    // balls.add(new Ball(400,10,10,10,this));
-    // balls.add(new Ball(300,30,10,200,this));
-    s = new Shapes();
-    add(s);
-    s.setVisible(true);
-      
+        canon = new Canon(this) ;
+        this.add(canon) ;
+        canon.setVisible(true);
+        canon.setBalleATirer(new Ball(0, 0, 0, 0, this));
+    
+    // Creation de de la Shape // paint les balles
+        s = new Shapes();
+        add(s);
+        s.setVisible(true);
+    
+   
     animate();
     
 
-    
 
     this.addMouseListener(this);
     this.addMouseMotionListener(this);
@@ -103,11 +92,8 @@ public class GameView extends JPanel implements MouseInputListener{
                 last = System.nanoTime();
                 for (Ball b:balls){
                     b.updateBall((last-now)*1.0e-9);
-                    // System.out.println(b.ballX);
                 }
-                // System.out.println(balls.size());
-                repaint();
-                s.repaint() ;
+                repaint() ;
                 now=last;
                 
             }
@@ -135,10 +121,13 @@ public class GameView extends JPanel implements MouseInputListener{
     
     
     public class Shapes extends JPanel{
+
+        Shapes(){
+            setSize(width,heigth);
+        }
         
         public void paint(Graphics g){
-            canon.repaint();
-            setSize(width,heigth);
+            super.paint(g);
             g.setColor(Color.BLACK);
             for (Ball ball:balls) g.fillOval((int)ball.ballX,(int)ball.ballY,(int)ball.ballRadius,(int)ball.ballRadius);
     
@@ -146,7 +135,6 @@ public class GameView extends JPanel implements MouseInputListener{
             g.setColor(Color.RED);
             for (Rectangle rect:rectanlgle) g.drawLine(rect.x0, rect.y0, rect.caculX1(), rect.caculY1());
     
-            System.out.println("in repaint");
         }
 
     }
@@ -190,14 +178,5 @@ public class GameView extends JPanel implements MouseInputListener{
         canon.DeplacementCanon(e);    
     } 
 
-    // public static void main(String[] args) {
-    //     JFrame f = new JFrame();
-    //     f.setSize(1000,1000);
-    //     f.setVisible(true);
-    //     f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //     f.setLayout(null);
-    //     GameView g = new GameView();
-    //     f.add(g);
-    //     System.out.println(g.getSize());
-    // }
+    
 }
