11d166329c3e710ade5b0d95add0db6a3deeb281
Thibault Poux <thibault.poux@etu.u-paris.fr>
2023-03-15 19:21:55 +0100

    Ajout des attributs des pegs dans la sauvegarde

---
+++ Modele/Pegs.java
@@ -0,0 +1,67 @@
+package Modele;
+
+public class Pegs {
+    private int x;
+    private int y;
+    private int radius;
+    private int couleur;
+    private String imageString;
+    private boolean toucher = false;
+    // int 1 à 4 couleur image 
+    public Pegs(int x, int y, int radius, int couleur) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.couleur = couleur;
+        if (couleur == 1) {
+            imageString = "blueball.png";
+        }
+        else if (couleur == 2) {
+            imageString = "redball.png";
+        }
+        else if (couleur == 3) {
+            imageString = "violetball.png";
+        }
+        else if (couleur == 4) {
+            imageString = "vertball.png";
+            
+        }
+    }
+    
+    public int getCouleur() {
+        return couleur;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getRadius() {
+        return radius;
+    }
+    public void toucher() {
+        toucher = true;
+    }
+    public boolean getHit() {
+        return toucher;
+    }
+    public boolean contains(int i, int j) {
+        if (Math.sqrt(Math.pow(i-x, 2)+Math.pow(j-y, 2))<=radius) {
+            return true;
+        }
+        return false;
+    }
+    public String getImageString() {
+        return imageString;
+    }
+    public String getImageStringTouche() {
+        if (couleur != 2){
+            return "redball.png";
+        } else {
+            return "violetball.png";
+        }
+    }
+    
+    
+}
--- Modele/Save.java
+++ Modele/Save.java
@@ -1,27 +1,19 @@
 package Modele;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.io.* ;
 
 
 // le code se trouvant ici sera surement déplacer vers une classe plus intérrésante 
 public class Save {
     
-    List<String> pegs ;
+    List<Pegs> listpegs ;
     String nomSauvegarde ;
-
-    
-    /*
-     * data type in csv file
-     * Premiére ligne avec la taille de l'air de jeu 
-     * court width ; court height; 
-     * 
-     * 
-     * 
-     * Peggles type ; width ; heigth ; pos x ; pos y ; ? color? ; resistance peegles ;
-     */
 
     
     public void save(String nomSauvegarde, int widthCourt, int heightCourt) throws FileNotFoundException{
@@ -41,12 +33,11 @@ public class Save {
         
 
         // remplacer par les valeurs à enregistrer
-        for (String peg : pegs){
-            ligne =  peg ;
-            // ligne  = String.valueOf(widthCourt) +";"
-            //             + String.valueOf(widthCourt) +";"
-            //             + String.valueOf(widthCourt) +";"
-            //             + String.valueOf(heightCourt) ;
+        for (Pegs peg : listpegs){
+            ligne  = String.valueOf(peg.getX()) +";" 
+                        + String.valueOf(peg.getY()) +";" 
+                        + String.valueOf(peg.getRadius()) +";" 
+                        + String.valueOf(peg.getCouleur()) ; ;
             file.println(ligne);
 
         }
@@ -54,32 +45,37 @@ public class Save {
         // enregistrement réussi
 
 
-
     }
     
-    public List<String> importPegles(String name, int widthCourt, int heightCourt) throws IOException{
-        List<String> pegs = new ArrayList<String>() ;
+    public List<Pegs> importPegles(String name, int widthCourt, int heightCourt) throws IOException{
+        List<Pegs> listpegs = new ArrayList<Pegs>() ;
 
        Scanner save  = new Scanner(new File("Modele/Map/" + nomSauvegarde + ".csv")) ;
 
        String[] line = save.nextLine().split(";") ;
        
-       // obtenir les valeurs de réajustement des pegs pour qu'il s'adepete à la nouvelle taille de l'écran 
+       // obtenir les valeurs de réajustement des des pegs pour qu'il s'adepete à la nouvelle taille de l'écran 
        double reajustementH = widthCourt / Double.valueOf(line[0]);
        double reajustementV = heightCourt /Double.valueOf(line[1]) ;
 
-        while(save.hasNextLine()){
+       // creation des pegs en fonction des infos que on a 
+      // update des nouveau coordonnées en fonction de la taille de l'écran actuel
+       while(save.hasNextLine()){
             line = save.nextLine().split(";") ;
 
-            // creation des pegs en fonction des infos que on a 
-            // update des nouveau coordonnées en fonction de la taille de l'écran actuel
+            int x =  (int) (reajustementH*Double.valueOf(line[0]) );
+            int y =  (int) (reajustementV*Double.valueOf(line[1]) );
+            int radius = (int)(Double.valueOf(line[2]) * Math.min(reajustementH, reajustementV) ) ;
+            int couleur = Integer.valueOf(line[3]) ;
+
+            listpegs.add(new Pegs(x, y, radius, couleur)) ;
 
         }
 
         save.close();
 
         // return un objet de type 
-        return pegs ;
+        return listpegs ;
     }
 
 }
