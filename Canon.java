ebda2266a978c7982a7bbb68b821f4cce09d200f
Albertu17 <tomasialbert3@gmail.com>
2023-03-20 11:08:39 +0100

    Maj editor

--- Canon.java
+++ Canon.java
@@ -1,8 +1,6 @@
-import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.Color;
 import java.awt.event.MouseEvent ;
 import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
---
+++ Niveau.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Niveau {
+    
+    private ArrayList<Pegs> pegs;
+    private final String nom;
+    private int nbBillesInitiales;
+    private int score1Etoile;
+    private int score2Etoiles;
+    private int score3Etoiles;
+
+    public Niveau (String nom) {
+        this.nom = nom;
+    }
+
+}
