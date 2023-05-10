1a14ca4d9e2a8ad0e25c4b8b991a8059d5e76b22
Thibault Poux <thibault.poux@etu.u-paris.fr>
2023-05-11 00:20:26 +0200

    psuh skin gauche

--- Vue/Menu/MenuParametres.java
+++ Vue/Menu/MenuParametres.java
@@ -24,17 +24,10 @@ public class MenuParametres extends JPanel {
     private int middleW;
 
     private BufferedImage background;
+    private JButton btnRetour;
 
-    private String[] allNameImage = new String[] { "Ball/ball.png", "Ball/basketBall.png", "Ball/smileysBall.png",
-            "Ball/soccerBall.png", "Ball/tennisBall.png" };
-    private BoutonBall[] tabBouton;
-    BoutonBall btnSkin1;
-    BoutonBall btnSkin2;
-    BoutonBall btnSkin3;
-    JButton btnRetour;
-    JButton skin;
-    JButton musicOn;
-    JButton musicOff;
+   
+    private Skin skin ; 
 
     JButton plus;
     JButton minus;
@@ -55,32 +48,7 @@ public class MenuParametres extends JPanel {
         // background
         background = ImageImport.getImage("Menu/menuBackground.jpg", width, height);
 
-        tabBouton = new BoutonBall[5];
 
-        // BoutonMenu skin1
-        tabBouton[0] = new BoutonBall(0, 50);
-        tabBouton[0].setLocation(middleW , middleH - 25 - 140);
-
-        add(tabBouton[0]);
-        
-        // BoutonMenu skin2
-        tabBouton[1] = new BoutonBall(1, 50);
-        tabBouton[1].setLocation(middleW , middleH - 25 - 70);
-        
-        add(tabBouton[1]);
-        // add(tab);
-        
-        // BoutonMenu skin3
-        tabBouton[2] = new BoutonBall(2, 50);
-        tabBouton[2].setLocation(middleW , middleH - 25);
-        add(tabBouton[2]);
-        tabBouton[3] = new BoutonBall(3, 50);
-        tabBouton[3].setLocation(middleW , middleH +35);
-        add(tabBouton[3]);
-        tabBouton[4] = new BoutonBall(4, 50);
-        tabBouton[4].setLocation(middleW , middleH +100);
-        add(tabBouton[4]);
-        // add(btnSkin3);
 
         // BoutonMenu back
         btnRetour = new BoutonMenu("Retour", 200, 50);
@@ -88,9 +56,8 @@ public class MenuParametres extends JPanel {
         btnRetour.addActionListener(e -> controleur.launchMenu());
         add(btnRetour);
 
-        setFocusable(true);
-        requestFocusInWindow();
-        addKeyListener(new BoutonMenu.BoutonClavier(new BoutonMenu[] {}, () -> controleur.launchMenu()));
+
+        skin = new Skin(WIDTH, HEIGHT)
 
         skin = new BoutonMenu("Skins",100,50);
         skin.setLocation(middleW-30, middleH-250);
@@ -136,74 +103,118 @@ public class MenuParametres extends JPanel {
         vitesse.setBounds(middleW - 500, middleH, 200, 50);
         add(vitesse);
 
-        iluminateButton();
+        setFocusable(true);
+        requestFocusInWindow();
+        addKeyListener(new BoutonMenu.BoutonClavier(new BoutonMenu[] {}, () -> controleur.launchMenu()));
     }
 
-    private void iluminateButton() {
-        for (int i = 0; i < allNameImage.length; i++) {
-            tabBouton[i].repaint();
-        }
-    }
-
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(background, 0, 0, this);
     }
+    
+    public class Skin extends JPanel{
+        private String[] allNameImage = new String[] { "Ball/ball.png", "Ball/basketBall.png", "Ball/smileysBall.png",
+        "Ball/soccerBall.png", "Ball/tennisBall.png" };
+        private BoutonBall[] tabBouton;
 
-    public class BoutonBall extends JButton {
 
-        int diametre, selecteur;
-        ImageIcon imageIconNormal;
-        ImageIcon imageIconOnHover;
-
-
-        public BoutonBall(int selecteur, int diametre) {
-            this.diametre = diametre;
-            this.selecteur = selecteur ;
-            String texteImage = allNameImage[selecteur];
-
-            BufferedImage tempNormal = ImageImport.getImage(texteImage, diametre, diametre);
-
-            BufferedImage tempHover = ImageImport.getImage("Ball/hoverBall.png", diametre, diametre);
-
-            Graphics g = tempHover.createGraphics();
-            g.drawImage(ImageImport.getImage(texteImage, diametre - 6, diametre - 6), 3, 3, this);
-
-            imageIconNormal = new ImageIcon(tempNormal);
-            imageIconOnHover = new ImageIcon(tempHover);
-
-            setIcon(imageIconNormal);
-            addMouseListener((MouseListener) new MouseAdapter() {
-                public void mouseEntered(MouseEvent evt) {
-                    setIcon(imageIconOnHover);
-                }
-
-                public void mouseExited(MouseEvent evt) { 
-                    setIcon(imageIconNormal);
-                }
-
-                public void mousePressed(MouseEvent evt) {
-                    Ball.setSelecteurImage(selecteur);
-                    Ball.setImage((ImageImport.getImage(texteImage, 20, 20)));
-                    iluminateButton() ;
-                }
-            });
-            // Parametrages du bouton
-            setBorderPainted(false);
-            setContentAreaFilled(false);
-            setFocusPainted(false);
+        Skin(int width, int height){
+            setSize(width, height);
             setOpaque(false);
-            setSize(diametre, diametre);
+            tabBouton = new BoutonBall[5];
+
+            // BoutonMenu skin1
+            tabBouton[0] = new BoutonBall(0, 50);
+            tabBouton[0].setLocation(middleW , middleH - 25 - 140);
+
+            add(tabBouton[0]);
+            
+            // BoutonMenu skin2
+            tabBouton[1] = new BoutonBall(1, 50);
+            tabBouton[1].setLocation(middleW , middleH - 25 - 70);
+            
+            add(tabBouton[1]);
+            // add(tab);
+            
+            // BoutonMenu skin3
+            tabBouton[2] = new BoutonBall(2, 50);
+            tabBouton[2].setLocation(middleW , middleH - 25);
+            add(tabBouton[2]);
+            tabBouton[3] = new BoutonBall(3, 50);
+            tabBouton[3].setLocation(middleW , middleH +35);
+            add(tabBouton[3]);
+            tabBouton[4] = new BoutonBall(4, 50);
+            tabBouton[4].setLocation(middleW , middleH +100);
+            add(tabBouton[4]);
+
+            iluminateButton() ;
         }
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            if (selecteur == Ball.getSelecteurImage()) {
-                g.setColor(Color.RED);
-                g.fillOval((this.diametre*4)/10, (this.diametre*4)/10, this.diametre/5, this.diametre/5);
+
+        private void iluminateButton() {
+            for (int i = 0; i < allNameImage.length; i++) {
+                tabBouton[i].repaint();
+            }
+        }
+
+        class BoutonBall extends JButton {
+
+            int diametre, selecteur;
+            ImageIcon imageIconNormal;
+            ImageIcon imageIconOnHover;
+    
+    
+            BoutonBall(int selecteur, int diametre) {
+                this.diametre = diametre;
+                this.selecteur = selecteur ;
+                String texteImage = allNameImage[selecteur];
+    
+                BufferedImage tempNormal = ImageImport.getImage(texteImage, diametre, diametre);
+    
+                BufferedImage tempHover = ImageImport.getImage("Ball/hoverBall.png", diametre, diametre);
+    
+                Graphics g = tempHover.createGraphics();
+                g.drawImage(ImageImport.getImage(texteImage, diametre - 6, diametre - 6), 3, 3, this);
+    
+                imageIconNormal = new ImageIcon(tempNormal);
+                imageIconOnHover = new ImageIcon(tempHover);
+    
+                setIcon(imageIconNormal);
+                addMouseListener((MouseListener) new MouseAdapter() {
+                    public void mouseEntered(MouseEvent evt) {
+                        setIcon(imageIconOnHover);
+                    }
+    
+                    public void mouseExited(MouseEvent evt) { 
+                        setIcon(imageIconNormal);
+                    }
+    
+                    public void mousePressed(MouseEvent evt) {
+                        Ball.setSelecteurImage(selecteur);
+                        Ball.setImage((ImageImport.getImage(texteImage, 20, 20)));
+                        iluminateButton() ;
+                    }
+                });
+                // Parametrages du bouton
+                setBorderPainted(false);
+                setContentAreaFilled(false);
+                setFocusPainted(false);
+                setOpaque(false);
+                setSize(diametre, diametre);
+            }
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                if (selecteur == Ball.getSelecteurImage()) {
+                    g.setColor(Color.RED);
+                    g.fillOval((this.diametre*4)/10, (this.diametre*4)/10, this.diametre/5, this.diametre/5);
+                }
             }
         }
     }
+
+    
 
 }
